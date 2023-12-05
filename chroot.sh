f08c112d278ce5a83b1c95757302276fc72cb8a9
pix-code <dbredemann25@mylcusd.net>
2023-12-04 16:15:20 -0800

    add stuff

--- chroot.sh
+++ chroot.sh
@@ -17,3 +17,12 @@ sed -i 's\#Color\Color\g' /etc/pacman.conf
 sed -i 's\#ParallelDownloads = 5\ParallelDownloads = 5\g' /etc/pacman.conf
 
 bootctl install
+
+echo "title   archlinux
+linux   /vmlinuz-linux
+initrd  /amd-ucode.img
+initrd  /initramfs-linux.img
+options root=$(blkid -o export /dev/vda2 | grep PARTUUID) rw" > /boot/loader/entries/arch.conf
+
+echo "default arch.conf
+timeout 0" > /boot/loader/loader.conf
--- user-chroot.sh
+++ user-chroot.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
-cd
-
 git clone https://aur.archlinux.org/paru-bin
-
-cd ~/paru-bin
-
+cd paru-bin
 makepkg -si
 
 paru -S pipewire xorg-server xorg-xinit plasma konsole blender fish eza micro network-manager-applet xf86-video-intel amd-ucode intel-ucode firewalld --noconfirm
