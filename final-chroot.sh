3dab93cef05e7073c2b933caeda871f402358cdd
pix-code <dbredemann25@mylcusd.net>
2023-12-05 13:09:59 -0800

    do stuff

--- final-chroot.sh
+++ final-chroot.sh
@@ -1,6 +1,6 @@
 # return askpass to sudoers
 sed -i 's\%wheel ALL=(ALL:ALL) NOPASSWD: ALL\# %wheel ALL=(ALL:ALL) NOPASSWD: ALL\g' /etc/sudoers
-sed -i 's\%wheel ALL=(ALL:ALL) ALL\# %wheel ALL=(ALL:ALL) ALL\g' /etc/sudoers
+sed -i 's\%# wheel ALL=(ALL:ALL) ALL\%wheel ALL=(ALL:ALL) ALL\g' /etc/sudoers
 
 # remove chroot scripts
 rm /chroot.sh
