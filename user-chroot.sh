76e75b2747467a7ea55bc503e23a13247480959a
pix-code <dbredemann25@mylcusd.net>
2023-12-05 15:53:00 -0800

    add stuff

--- user-chroot.sh
+++ user-chroot.sh
@@ -13,8 +13,26 @@ paru -S pipewire xorg-server xorg-xinit plasma konsole blender fish micro networ
 # remove unneeded stuff (very innefficent but im lazy)
 paru -Rns discover flatpak-kcm breeze-plymouth oxygen plasma-vault plasma-welcome plasma-workspace-wallpapers plymouth-kcm sddm-kcm --noconfirm
 
+echo "[Desktop Entry]
+Comment[en_US]=
+Comment=
+Exec=blender
+GenericName[en_US]=
+GenericName=
+Icon=blender
+MimeType=
+Name[en_US]=blender
+Name=blender
+Path=
+StartupNotify=true
+Terminal=false
+TerminalOptions=
+Type=Application
+X-KDE-SubstituteUID=false
+X-KDE-Username=" > ~/Desktop/blender.desktop
+
 # add .xinitrc
-echo startplasma-x11 > .xinitrc
+echo "startplasma-x11" > ~/.xinitrc
 
 # set bash_profile to automatically start xinit
 echo '#
@@ -25,5 +43,5 @@ echo '#
 
 if [ -z "${DISPLAY}" ] && [ "${XDG_VTNR}" -eq 1 ]; then
   exec startx
-fi' > .bash_profile
+fi' > ~/.bash_profile
 
